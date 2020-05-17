c87ba51d42d24d635518afbfe0787c8476a8a8da
Anna <anna.antip34@gmail.com>
2020-05-17 06:32:42 +0300

    Add project files.

---
+++ OOP_lab3_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("file1.txt");
+
+            string str = fromFile.ReadToEnd();
+
+            StreamWriter toFile = new StreamWriter("output.txt");
+
+            toFile.WriteLine(str);
+
+            fromFile = new StreamReader("file2.txt");
+
+            str = fromFile.ReadToEnd();
+
+            toFile.Write(str);
+
+            toFile.Close();
+        }
+    }
+}
